0acd93161d65f7288922ba5707cecd0f9c1f0381
alessandropisent <alessandro.pisent@hotmail.com>
2023-10-16 18:05:08 +0200

    commit

--- app.py
+++ app.py
@@ -11,35 +11,48 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/<role>/upload', methods=['POST'])
 def upload_image(role):
+    
     try:
+        if role == "farmer":
+                
+            data = request.json  # Assuming JSON request with base64 data
+            
+            base64_image = data['base64_image']
+            user = data['user']
+            
+            path_folder = UPLOAD_FOLDER + "/farmer/" + user
+            
+            if not os.path.exists(path_folder):
+                os.makedirs(path_folder)
+            
+            # Decode the base64 image data
+            image_data = base64.b64decode(base64_image)
+            
+            # Create a unique filename for the image
+            filename = f'image_{len(os.listdir(path_folder)) + 1}.png'
+            filepath = os.path.join(path_folder, filename)
+            
+            # Save the decoded image to the 'uploads' directory
+            with open(filepath, 'wb') as image_file:
+                image_file.write(image_data)
+            
+            # Construct the URL for the uploaded image
+            image_url = f"images/{role}/{user}/{filename}"
         
-        data = request.json  # Assuming JSON request with base64 data
-
-        base64_image = data['base64_image']
-        user = data['user']
+            return jsonify({'image_url': image_url})
         
-        # Decode the base64 image data
-        image_data = base64.b64decode(base64_image)
-        
-        # Create a unique filename for the image
-        filename = f'image_{len(os.listdir(app.config["UPLOAD_FOLDER"])) + 1}.png'
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        
-        # Save the decoded image to the 'uploads' directory
-        with open(filepath, 'wb') as image_file:
-            image_file.write(image_data)
-        
-        # Construct the URL for the uploaded image
-        image_url = f"/{UPLOAD_FOLDER}/{filename}"
-        
-        return jsonify({'image_url': image_url})
+        return jsonify({'image_url': "errore"})
+    
     except Exception as e:
         return jsonify({'error': str(e)})
 
-@app.route('/images/<image_filename>', methods=['GET'])
-def get_image(image_filename):
+@app.route('/images/<role>/<user>/<image_filename>', methods=['GET'])
+def get_image(role,user,image_filename):
+    
+    path_folder = f"{UPLOAD_FOLDER}/{role}/{user}"
+    
     # Construct the path to the image
-    image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_filename)
+    image_path = os.path.join(path_folder, image_filename)
     
     # Serve the image using send_file
     return send_file(image_path, as_attachment=False)
